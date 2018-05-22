910f301aa31b253fa7b9d87f454ad9e7cd89c405
Mohanasundar.M <mmsmohanece@gmail.com>
2018-05-22 11:14:19 +0530

    Create 112.py

---
+++ 112.py
@@ -0,0 +1,11 @@
+a= int(input("size"))
+e= int(input("compare"))
+b=[]
+for i in range(0,a):
+  c=int(input(""))
+  b.append(c)
+for i in b :
+  if(i==e):
+    print("yes")
+  else:
+    print("no")
